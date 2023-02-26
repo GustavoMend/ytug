7b72d044cc039920c051d8f78c9594a333f78870
GustavoMend <73637609+GustavoMend@users.noreply.github.com>
2023-02-25 23:10:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 import os
 import pytube
 from moviepy.editor import *
-import mutagen
-
-# Define the update_metadata function
-def update_metadata(file_path: str, title: str, artist: str, album: str="") -> None:
-    # Update the file metadata according to YouTube video details
-    with open(file_path, 'r+b') as file:
-        media_file = mutagen.File(file, easy=True)
-        media_file["title"] = title
-        media_file["artist"] = artist
-        media_file["album"] = album
-        media_file.save()
 
 # Ask user for YouTube video URL
 url = input("Enter YouTube video URL: ")
@@ -27,14 +16,17 @@ output_mp3 = f"{yt.title}.mp3"
 # Download audio stream to MP4 file
 audio_stream.download(output_path=os.getcwd(), filename=output_mp4)
 
-# Convert MP4 file to MP3 file
-print("Converting audio file...")
-with AudioFileClip(output_mp4) as audio_clip:
-    audio_clip.write_audiofile(output_mp3, bitrate="192k")
-print("Conversion complete.")
+# Convert MP4 file to MP3 file and add metadata
+audio_clip = AudioFileClip(output_mp4)
+audio_clip.write_audiofile(output_mp3, bitrate="192k")
+audio_clip.close()
 
-# Update file metadata
-update_metadata(output_mp3, yt.title, yt.author)
+# Add metadata to the output MP3 file
+audio = MP3(output_mp3, ID3=ID3)
+audio.tags.add(TIT2(text=yt.title))
+audio.tags.add(TPE1(text=yt.author))
+audio.tags.add(TALB(text=yt.title))
+audio.save()
 
 # Delete MP4 file
 os.remove(output_mp4)
