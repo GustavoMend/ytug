e5b7a43b60a4839357c6b0acd711c731b04f470c
GustavoMend <73637609+GustavoMend@users.noreply.github.com>
2023-02-25 22:57:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -21,10 +21,10 @@ print("Converting audio file...")
 with AudioFileClip(output_mp4) as audio_clip:
     audio_clip.write_audiofile(output_mp3, bitrate="192k")
 print("Conversion complete.")
-
+def update_metadata(file_path: str, title: str, artist: str, album: str="") -> None:
 # Update file metadata
 update_metadata(output_mp3, yt.title, yt.author)
-def update_metadata(file_path: str, title: str, artist: str, album: str="") -> None:
+
     # Update the file metadata according to YouTube video details
     with open(output_mp3, 'r+b') as file:
         media_file = mutagen.File(file, easy=True)
