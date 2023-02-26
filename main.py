2bc6a6fc24a0b0ce4ae98fda94e2d1e1e9a13d00
GustavoMend <73637609+GustavoMend@users.noreply.github.com>
2023-02-25 23:33:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,37 @@
 import os
 import pytube
-from moviepy.editor import *
-from mutagen.mp3 import MP3, ID3, TIT2, TPE1, TALB
+from moviepy.editor import AudioFileClip
+import mutagen
 
-# Ask user for YouTube video URL
-url = input("Enter YouTube video URL: ")
+def download_audio(url):
+    # Create YouTube object and extract audio stream
+    yt = pytube.YouTube(url)
+    audio_stream = yt.streams.filter(only_audio=True).first()
 
-# Create YouTube object and extract audio stream
-yt = pytube.YouTube(url)
-audio_stream = yt.streams.filter(only_audio=True).first()
+    # Set output file names
+    output_mp4 = f"{yt.title}.mp4"
+    output_mp3 = f"{yt.title}.mp3"
 
-# Set output file names
-output_mp4 = f"{yt.title}.mp4"
-output_mp3 = f"{yt.title}.mp3"
+    # Download audio stream to MP4 file
+    audio_stream.download(output_path=os.getcwd(), filename=output_mp4)
 
-# Download audio stream to MP4 file
-audio_stream.download(output_path=os.getcwd(), filename=output_mp4)
+    # Use moviepy to convert an mp4 to an mp3 with metadata support. Delete mp4 afterwards
+    audio_clip = AudioFileClip(output_mp4)
+    output_mp3_with_metadata = output_mp3.replace(".mp3", "-with-metadata.mp3")
+    audio_clip.write_audiofile(output_mp3_with_metadata)
+    audio_clip.close()
+    os.remove(output_mp4)
 
-# Convert MP4 file to MP3 file and add metadata
-audio_clip = AudioFileClip(output_mp4)
-audio_clip.write_audiofile(output_mp3, bitrate="192k")
-audio_clip.close()
+    update_metadata(output_mp3_with_metadata, yt.title, yt.author)
 
-# Add metadata to the output MP3 file
-audio = MP3(output_mp3, ID3=ID3)
-audio["TIT2"] = TIT2(encoding=3, text=yt.title)
-audio["TPE1"] = TPE1(encoding=3, text=yt.author)
-audio["TALB"] = TALB(encoding=3, text=yt.title)
-audio.save()
+def update_metadata(file_path, title, artist, album=""):
+    # Update the file metadata according to YouTube video details
+    with open(file_path, 'r+b') as file:
+        media_file = mutagen.File(file, easy=True)
+        media_file["title"] = title
+        if album: media_file["album"] = album
+        media_file["artist"] = artist
+        media_file.save()
 
 
-# Delete MP4 file
-os.remove(output_mp4)
-
-print(f"Audio saved to {output_mp3}")
+print(Audio saved)
