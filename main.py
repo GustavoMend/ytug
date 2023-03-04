b0a3c307011d9887f1304b7d21b6bdf6aee5ee42
GustavoMend <73637609+GustavoMend@users.noreply.github.com>
2023-03-04 16:59:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from moviepy.editor import AudioFileClip
 import pytube
 import os
 import mutagen
-
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! Please send me a YouTube video URL to download the audio.")
 
 def download_and_convert(url):
     # Create YouTube object and extract audio stream
     try:
         yt = pytube.YouTube(url)
     except pytube.exceptions.RegexMatchError:
-        return "The provided URL is not supported."
+        print("The provided URL is not supported.")
+        return
     audio_stream = yt.streams.filter(only_audio=True).first()
     # Set output file names
     output_mp4 = f"{yt.title}.mp4"
@@ -35,7 +32,7 @@ def download_and_convert(url):
     # Update metadata of MP3 file
     update_metadata(output_mp3, title, artist, album)
 
-    return output_mp3
+    print(f"Download complete. Audio saved as {output_mp3}.")
 
 def update_metadata(file_path: str, title: str, artist: str, album: str="") -> None:
     # Update the file metadata according to YouTube video details
@@ -47,29 +44,5 @@ def update_metadata(file_path: str, title: str, artist: str, album: str="") -> N
         media_file["artist"] = artist
         media_file.save(file)
 
-def handle_message(update, context):
-    text = update.message.text
-    if "youtube.com" in text:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Downloading...")
-        output_mp3 = download_and_convert(text)
-        if output_mp3:
-            context.bot.send_audio(chat_id=update.effective_chat.id, audio=open(output_mp3, 'rb'))
-            os.remove(output_mp3)
-        else:
-            context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I couldn't download that video.")
-
-
-def main():
-    updater = Updater(token="5542310588:AAEIZ8lDrbRyQGtjOG33Tf8Ly_lpiLBwYsk", use_context=True)
-
-    start_handler = CommandHandler('start', start)
-    message_handler = MessageHandler(Filters.text, handle_message)
-
-    updater.dispatcher.add_handler(start_handler)
-    updater.dispatcher.add_handler(message_handler)
-
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+url = input("Enter YouTube video URL: ")
+download_and_convert(url)
