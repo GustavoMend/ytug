c19b5f21646bfca3945460ece7e8b71278b485d4
GustavoMend <73637609+GustavoMend@users.noreply.github.com>
2023-03-04 15:26:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,42 @@
-from moviepy.editor import AudioFileClip
-import pytube
 import os
+import telethon.sync
+from telethon import TelegramClient, events, functions, types
+from pytube import YouTube
+from moviepy.editor import *
 import mutagen
 
-def download_and_convert(url):
-    # Create YouTube object and extract audio stream
+# replace the values below with your own
+api_id = 11169140
+api_hash = '4b185d543b0d1a84bed3a462ade1498f'
+bot_token = '5542310588:AAEIZ8lDrbRyQGtjOG33Tf8Ly_lpiLBwYsk'
+
+client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
+
+@client.on(events.NewMessage(pattern='^/start'))
+async def start(event):
+    await event.respond('Hello, please send me a YouTube link.')
+
+@client.on(events.NewMessage)
+async def convert_video(event):
     try:
-        yt = pytube.YouTube(url)
-    except pytube.exceptions.RegexMatchError:
-        print("The provided URL is not supported.")
-        return
-    audio_stream = yt.streams.filter(only_audio=True).first()
-    # Set output file names
-    output_mp4 = f"{yt.title}.mp4"
-
-    # Download audio stream to MP4 file
-    audio_stream.download(output_path=os.getcwd(), filename=output_mp4)
-
-    # Use moviepy to convert the MP4 file to an MP3 file with metadata support, then delete the MP4 file
-    audio_clip = AudioFileClip(output_mp4)
-    output_mp3 = f"{yt.title}.mp3"
-    audio_clip.write_audiofile(output_mp3)
-    audio_clip.close()
-    os.remove(output_mp4)
-
-    # Get video details from YouTube
-    title = yt.title
-    artist = yt.author
-    album = ""
-
-    # Update metadata of MP3 file
-    update_metadata(output_mp3, title, artist, album)
-
-    print(f"Download complete. Audio saved as {output_mp3}.")
+        url = event.message.text
+        yt = YouTube(url)
+        audio_stream = yt.streams.filter(only_audio=True).first()
+        output_mp4 = f"{yt.title}.mp4"
+        audio_stream.download(output_path='./downloads', filename=output_mp4)
+        output_mp3 = f"{yt.title}.mp3"
+        audio_clip = AudioFileClip(os.path.join('./downloads', output_mp4))
+        audio_clip.write_audiofile(os.path.join('./downloads', output_mp3))
+        audio_clip.close()
+        os.remove(os.path.join('./downloads', output_mp4))
+        title = yt.title
+        artist = yt.author
+        album = ""
+        update_metadata(os.path.join('./downloads', output_mp3), title, artist, album)
+        await client.send_file(event.chat_id, os.path.join('./downloads', output_mp3))
+    except Exception as e:
+        print(e)
+        await event.respond('Sorry, an error occurred while processing your request.')
 
 def update_metadata(file_path: str, title: str, artist: str, album: str="") -> None:
     # Update the file metadata according to YouTube video details
@@ -44,5 +48,4 @@ def update_metadata(file_path: str, title: str, artist: str, album: str="") -> N
         media_file["artist"] = artist
         media_file.save(file)
 
-url = input("Enter YouTube video URL: ")
-download_and_convert(url)
+client.run_until_disconnected()
