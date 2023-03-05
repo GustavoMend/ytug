77d0e20768e1373d97ae6f3b708850fae0db2e07
GustavoMend <73637609+GustavoMend@users.noreply.github.com>
2023-03-04 19:59:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from moviepy.editor import AudioFileClip
 import pytube
-
+import os
 import mutagen
 
 def download_and_convert(url):
@@ -15,14 +15,14 @@ def download_and_convert(url):
     output_mp4 = f"{yt.title}.mp4"
 
     # Download audio stream to MP4 file
-    audio_stream.download(output_path=getcwd(), filename=output_mp4)
+    audio_stream.download(output_path=os.getcwd(), filename=output_mp4)
 
     # Use moviepy to convert the MP4 file to an MP3 file with metadata support, then delete the MP4 file
     audio_clip = AudioFileClip(output_mp4)
     output_mp3 = f"{yt.title}.mp3"
     audio_clip.write_audiofile(output_mp3)
     audio_clip.close()
-    remove(output_mp4)
+    os.remove(output_mp4)
 
     # Get video details from YouTube
     title = yt.title
@@ -46,4 +46,3 @@ def update_metadata(file_path: str, title: str, artist: str, album: str="") -> N
 
 url = input("Enter YouTube video URL: ")
 download_and_convert(url)
-
